78e2b7fef5b153ba1c8e830fc842c5299f726b45
Eduardo Lemos <103197804+EduLemos0@users.noreply.github.com>
2022-07-02 16:41:48 -0300

    Update Med.Aritmetica.js

--- Med.Aritmetica.js
+++ Med.Aritmetica.js
@@ -1,19 +1,18 @@
-var convidados = [];
+var numeros = [];
 
 while(true) {
-    var nome = prompt("Digite o nome do convidado");
-
-    if(nome == "S"){
+    var numero = prompt("Digite os números desejados ou escreva S para sair.")
+    if (numero == "S") {
         break;
     } else {
-         var idade = Number(prompt("Digite a idade do convidado"));
-					if(idade < 18){
-            alert("Você não tem a idade mínima permitda para participar do evento");
-						break;
-        }
-        var cpf = prompt("Digite o CPF do convidado");
-        convidados.push({"nome":nome, "idade":idade, "CPF":cpf});
-	    }
-    } 
+        numeros.push(Number(numero));
+    }
+}
+var soma = 0;
+for (i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
+}
 
-    console.log(convidados);
+resultado = soma / numeros.length;
+
+console.log(resultado);
